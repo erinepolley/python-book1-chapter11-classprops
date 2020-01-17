ae2420ae4932ff7c428bd71f9a7a82a17e9a3c60
Erin Polley <erinepolley@gmail.com>
2020-01-17 13:43:51 -0600

    DONE

--- sensitiveinfo.py
+++ sensitiveinfo.py
@@ -2,29 +2,52 @@
 #first name, last name--cannot GET
 #address: can both GET and SET
 class Patient():
-    def __init__(self, birth, insurance):
-        self.social = "666-66-6666"
-        self.birth = birth
-        self.insurance = insurance 
-        # self.full_name = self.first_name + self.last_name 
-
+    def __init__(self, social, birth, insurance, first_name, last_name, address):
+        self.__social = social
+        self.__birth = birth
+        self.__insurance = insurance 
+        self.__address = address
+#fullname = firstname + lastname
     # def full_name(self):
     #     return (f"{first_name}  {last_name}")
 
 #SSN
-    @property # The getter
-    def social(self):
-        try:
-            return self.__social # Note there are 2 underscores here
-        except AttributeError:
-            return "No SSN"
-
     # @property # The getter
     # def social(self):
     #     try:
     #         return self.__social # Note there are 2 underscores here
     #     except AttributeError:
     #         return "No SSN"
+
+    # @property
+    # def full_name(self):
+    #     try:
+    #         return f"{self.first_name} {self.last_name}"
+    #     except AttributeError:
+    #         return "no full name, sry"
+
+# ADDRESS GETTER
+    @property 
+    def address(self):
+        try:
+            return self.__address
+        except AttributeError:
+            return "Please enter an address"
+
+    @address.setter # The setter
+    def address(self, address):
+        if type(address) is str:
+            self.__address = address
+        else:
+            raise TypeError('Please enter a number')
+#SSN!!!!!!!!!!!!!!!!!!!!!!!!!
+    @property 
+    def social(self):
+        try:
+            return self.__social # Note there are 2 underscores here
+        except AttributeError:
+            return "No SSN"
+
     # @first_name.setter # The setter
     # def first_name(self, first_name):
     #     if type(first_name) is str:
@@ -32,6 +55,6 @@ class Patient():
     #     else:
     #         raise TypeError('Please provide a name')
 
-erin = Patient()
+erin = Patient("666-66-6666", "11/06/85", 666, "Erin", "Polley", "666 Satan St")
 
 print(erin.social)
